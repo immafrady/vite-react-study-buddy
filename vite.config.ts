af6410ce409e1aebb6aa80f6dcc4599b96e349d8
Frady Wang <immafrady@gmail.com>
2024-03-19 22:02:47 +0800

    chore: Edit deploy.yml - 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+// @ts-ignore
+import pkg from './package.json';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `/${pkg.name}/`,
   build: {
     target: 'es2015'
   },
