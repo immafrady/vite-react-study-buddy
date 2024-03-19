821a19f8465da5fbda46f196b84a606cb440ca77
Frady Wang <immafrady@gmail.com>
2024-03-19 11:46:15 +0800

    chore: 修改build.target

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2015'
+  },
   plugins: [react()],
   resolve: {
     alias: {
